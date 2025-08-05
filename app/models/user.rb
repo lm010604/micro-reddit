08c26cdf5c213ff166a7408ed8b46262f8793d32
Lauren Mok <lm685@cornell.edu>
2025-08-05 12:26:53 +0800

    Created Post and Comment models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
   validates :username, uniqueness: true, length: { in: 3..20 }, presence: true
   validates :email, presence: true, length: { maximum: 255 }, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
